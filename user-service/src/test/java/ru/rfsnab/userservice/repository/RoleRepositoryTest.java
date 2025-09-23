c47c39902b6e1cff3d6e2defb710d82abc38d945
elims777 <elims777@gmail.com>
2025-09-23 07:58:57 +0300

    config

---
+++ user-service/src/test/java/ru/rfsnab/userservice/repository/RoleRepositoryTest.java
@@ -0,0 +1,16 @@
+package ru.rfsnab.userservice.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+//@DataJpaTest
+class RoleRepositoryTest {
+
+//    @Autowired
+    private RoleRepository roleRepository;
+
+//    @Test
+    void findByNameIfExistReturnRole() {
+
+    }
+}
